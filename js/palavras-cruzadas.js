ffa177109d3e384477f0fd4fc07e52badb08416a
Felipe Marchi <fel.marchi@gmail.com>
2018-11-14 00:20:30 -0200

    Initial commit

---
+++ js/palavras-cruzadas.js
@@ -0,0 +1,7 @@
+function btnNovoJogo() {
+    var form1 = document.getElementById("form-cadastro")
+    var form2 = document.getElementById("form-jogo")
+
+    form1.setAttribute("class","d-none")
+    form2.removeAttribute("class")
+}
